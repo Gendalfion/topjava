98450cc2db76fbf2ea3d4c64220d1b9a1cadac58
Amvrosov <amvrosovalex@gmail.com>
2017-09-13 00:12:09 +0800

    HW07 1.1: delete/load meals table ajax implementation on server side

---
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
@@ -0,0 +1,40 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.to.MealWithExceed;
+import ru.javawebinar.topjava.web.user.AbstractUserController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ajax/profile/meals")
+public class MealAjaxController extends AbstractMealController {
+
+
+    @Override
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<MealWithExceed> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") int id) {
+        super.delete(id);
+    }
+
+    /*@PostMapping
+    public void createOrUpdate(@RequestParam("id") Integer id,
+                               @RequestParam("name") String name,
+                               @RequestParam("email") String email,
+                               @RequestParam("password") String password) {
+
+        User user = new User(id, name, email, password, Role.ROLE_USER);
+        if (user.isNew()) {
+            super.create(user);
+        }
+    }*/
+}
